8ef9603d12489e06f2a3486e81e10ced573b953c
AlexMusin <alersky21@gmail.com>
2023-10-14 23:07:11 +0300

    add check to test_start_page.py

--- tests/test_start_page.py
+++ tests/test_start_page.py
@@ -2,9 +2,13 @@ import pytest
 import page_analyzer.app as app
 
 
-def test_start_page():
+def test_start_page_response():
     response = app.test_client().get('/')
     assert response.status_code == 200
+
+def test_start_page_body():
+    response = app.test_client().get('/')
+    assert "Анализатор страниц" in response.text
 
 
 
