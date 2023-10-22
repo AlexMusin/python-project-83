8fa04cf4ee51168efcd010c9dcd0f10de8322fd6
AlexMusin <alersky21@gmail.com>
2023-10-22 16:52:02 +0300

    add database.sql

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,9 +1,14 @@
+import psycopg2
+import os
 from flask import Flask, render_template
 from dotenv import load_dotenv
 
 load_dotenv()
 app = Flask(__name__)
 
+DATABASE_URL = os.getenv('DATABASE_URL')
+#print(DATABASE_URL)
+conn = psycopg2.connect(DATABASE_URL)
 
 @app.route('/')
 def start_page():
---
+++ page_analyzer/database.sql
@@ -0,0 +1,5 @@
+CREATE TABLE urls (
+    id bigint GENERATED ALWAYS AS IDENTITY,
+    name varchar,
+    created_at datetime
+)
